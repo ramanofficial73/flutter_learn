c10b519cf20dc6ef2b9afa2c32a894b5b934b76b
Raman Sharma <rksraman73@gmail.com>
2022-08-26 22:01:28 +0530

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text('Hell'),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
